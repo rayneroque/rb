4fbe77655f5756cc481a6d0e7c8d216ccf6ec722
rayneroque <roque.raynebernadette@ue.edu.ph>
2025-02-12 07:53:48 +0800

    Update rayne.cpp

--- rayne.cpp
+++ rayne.cpp
@@ -3,38 +3,36 @@
 using namespace std;
 
 int main() {
-    int height = 6;  // Height of the house
-    int width = 11;  // Width of the house (it should be an odd number for symmetry)
+    int height = 6;  
+    int width = 11; 
     
-    // Roof of the house (triangular shape)
     for (int i = 0; i < height; i++) {
         for (int j = 0; j < width; j++) {
             if (j >= (width / 2 - i) && j <= (width / 2 + i)) {
-                cout << "#";  // Part of the roof made of '#'
+                cout << "#";  
             } else {
-                cout << " ";  // Space to create the triangle shape
+                cout << " ";  
             }
         }
         cout << endl;
     }
 
-    // Walls of the house
-    for (int i = 0; i < height - 2; i++) {  // Height of the walls (height - roof)
+    for (int i = 0; i < height - 2; i++) {  
         for (int j = 0; j < width; j++) {
             if (j == 0 || j == width - 1) {
-                cout << "|";  // Left and right walls of the house
+                cout << "|";  
             } else if (i == height - 3 && j == width / 2) {
-                cout << "*";  // Door in the middle of the house
+                cout << "*";  
             } else {
-                cout << " ";  // Inside the house (empty space)
+                cout << " ";  
             }
         }
         cout << endl;
     }
 
-    // Base of the house (foundation)
+  
     for (int i = 0; i < width; i++) {
-        cout << "_";  // Foundation of the house
+        cout << "_";
     }
     cout << endl;
 
