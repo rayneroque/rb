2ba0f47197897c2ce373b3f423050da5c5fe528e
rayneroque <roque.raynebernadette@ue.edu.ph>
2025-02-17 21:51:27 +0800

    Update rayne.cpp

--- rayne.cpp
+++ rayne.cpp
@@ -1,40 +1,16 @@
-#include <iostream>
-
+#include<iostream>
 using namespace std;
-
-int main() {
-    int height = 6;  
-    int width = 11; 
-    
-    for (int i = 0; i < height; i++) {
-        for (int j = 0; j < width; j++) {
-            if (j >= (width / 2 - i) && j <= (width / 2 + i)) {
-                cout << "#";  
-            } else {
-                cout << " ";  
-            }
-        }
-        cout << endl;
-    }
-
-    for (int i = 0; i < height - 2; i++) {  
-        for (int j = 0; j < width; j++) {
-            if (j == 0 || j == width - 1) {
-                cout << "|";  
-            } else if (i == height - 3 && j == width / 2) {
-                cout << "*";  
-            } else {
-                cout << " ";  
-            }
-        }
-        cout << endl;
-    }
-
-  
-    for (int i = 0; i < width; i++) {
-        cout << "_";
-    }
-    cout << endl;
-
-    return 0;
+main()
+{
+int a[10];
+int ctr;
+for(ctr=0;ctr<=9;ctr++)
+{
+cout<<"Enter any Number= ";
+cin>>a[ctr];
+}
+cout<<“OUTPUT IN REVERSE ORDER”<<endl;
+for(ctr=9;ctr>=0;ctr--)
+cout<<a[ctr]<<endl;
+return 0;
 }
