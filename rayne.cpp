1f3d69371c7af4c0695d74d4e25e8a4bf721fc79
rayneroque <roque.raynebernadette@ue.edu.ph>
2025-02-22 20:06:30 +0800

    Update rayne.cpp

--- rayne.cpp
+++ rayne.cpp
@@ -1,17 +1,27 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-main()
-{
-int a[15];
-int ctr;
-for(ctr=0;ctr<=14;ctr++)
-{
-cout<<"Enter any Number = ";
-cin>>a[ctr];
+
+// Function to calculate retail price
+double calculateRetailPrice(double wholesaleCost, double markupPercentage) {
+    return wholesaleCost + (wholesaleCost * markupPercentage / 100);
 }
-cout<<“LIST OF EVEN NUMBERS”<<endl;
-for(ctr=0;ctr<=14;ctr++)
-if(a[ctr] % 2 == 0)
-cout<<a[ctr]<<endl;
-return 0;
+
+int main() {
+    double wholesaleCost, markupPercentage;
+
+    // Ask for wholesale cost
+    cout << "Enter the item's wholesale cost: ";
+    cin >> wholesaleCost;
+
+    // Ask for markup percentage
+    cout << "Enter the item's markup percentage: ";
+    cin >> markupPercentage;
+
+    // Calculate retail price
+    double retailPrice = calculateRetailPrice(wholesaleCost, markupPercentage);
+
+    // Display retail price
+    cout << "The item's retail price is: $" << retailPrice << endl;
+
+    return 0;
 }
